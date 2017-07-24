e13af97271689541402488f8a7cad89889b8699d
Janak <janakrule@gmail.com>
2017-07-24 13:54:47 +0530

    first commit

---
+++ email.php
@@ -0,0 +1,52 @@
+<?php
+require 'mailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'janakrule@gmail.com';                 // SMTP username
+$mail->Password = '9928323998janak';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('janakrule@gmail.com', 'Mailer');
+$mail->addAddress('sahilkk555@gmail.com', 'Sahil');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Congrats';
+$mail->Body    = 'You got the <b>CDI</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
+
+<html>
+
+	<head>
+    	<title>Sending HTML email using PHP</title>
+    </head>
+    
+    <body>
+    	<form>
+			<button >Send</button>
+        </form>
+    </body>
+
+</html>
